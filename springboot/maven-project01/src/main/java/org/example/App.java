79e57619d44f1279954383686b3454f50c523a08
shendeming <38714822+shendeming@users.noreply.github.com>
2023-08-20 09:51:59 +0800

    springboot_1.0

---
+++ springboot/maven-project01/src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import ch.qos.logback.classic.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        Logger log;
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ springboot/maven-project01/src/test/java/org/example/AppTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import ch.qos.logback.classic.Logger;
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest{
+    @Test
+    public void test1(){
+        Logger log;
+        System.out.println("hello Test1");
+    }
+
+    @Test
+    public void test2(){
+        Logger log;
+        System.out.println("hello Test2");
+    }
+
+    @Test
+    public void test3(){
+        Logger log;
+        System.out.println("hello Test3");
+    }
+}
